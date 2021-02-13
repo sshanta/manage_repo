8d1a721597ded7336695e2ee43f0256900d78292
sshanta <78470513+sshanta@users.noreply.github.com>
2021-02-13 20:43:52 +0000

    update letblink.py using PWM

--- ledBlink.py
+++ ledBlink.py
@@ -2,6 +2,8 @@ import time             # image time library
 import RPi.GPIO as GPIO # import python package for GPIO's
 GPIO.setmode(GPIO.BCM) # Sets Pin Configuration
 GPIO.setup(2,GPIO.OUT) # Sets Output Pin
+LED = GPIO.PWM(2, 50)
+LED.start(0)
 try:
 	while(1):
 		GPIO.output(2,GPIO.HIGH) # Logic 1 on Pin
@@ -10,4 +12,5 @@ try:
 		time.sleep(1)
 except KeyboardInterrupt:
 	pass
-GPIO.cleanup()
+p.stop()
+GPIO.cleanup()  #leaving GPIO's at their default values
